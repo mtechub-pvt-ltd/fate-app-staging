f68f77bbf29ff0dea1d0ddf8a167e492d11eb2e0
mtechub-pvt-ltd <mtechub.info@gmail.com>
2024-12-29 04:10:45 +0500

    code push for distribution

--- app/src/views/screens/Auth/SignUp/SignUp_N.js
+++ app/src/views/screens/Auth/SignUp/SignUp_N.js
@@ -67,6 +67,7 @@ function SignUp_N({ route, navigation }) {
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexPattern.test(String(email).toLowerCase());
   }
+  // test 1-11
 
   function validateStrongPassword(password) {
     const minLength = 8;
